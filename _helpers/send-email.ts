fac37de61551b4ffd4bfe207adb2defda48d4d18
epica <epicarusracoma@gmail.com>
2026-05-16 22:24:19 +0800

    backend prep

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import config from '../config';
+import config from '../config.json';
 
 export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
     const transporter = nodemailer.createTransport(config.smtpOptions);
