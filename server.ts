5e7862ca7eb9cc723a82768e4f98ff187a79250c
epica <epicarusracoma@gmail.com>
2026-05-18 20:25:38 +0800

    cross-site cookies

--- server.ts
+++ server.ts
@@ -15,7 +15,9 @@ app.use(cookieParser());
 // allow cors requests from any origin and with credentials
 const corsOptions = {
     origin: process.env.CORS_ORIGIN || true,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 app.use(cors(corsOptions));
 
