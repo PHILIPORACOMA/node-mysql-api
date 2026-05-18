54414abf5a077cda05c9091b11aa48633271a8d3
epica <epicarusracoma@gmail.com>
2026-05-18 17:36:41 +0800

    jwt fixes

--- _middleware/authorize.ts
+++ _middleware/authorize.ts
@@ -1,8 +1,8 @@
 import { expressjwt as jwt } from 'express-jwt';
-import config from '..//config.json';
+import config from '../config.json';
 import db from '../_helpers/db.ts';
 
-const { secret } = config;
+const secret = process.env.JWT_SECRET || config.secret;
 
 export default function authorize(roles: any = []) {
     if (typeof roles === 'string') {
